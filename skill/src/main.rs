9179f53439a45148a6716d8df69c212a582ac7dc
Kesavan Yogeswaran <kesyog@gmail.com>
2020-06-07 17:41:36 -0400

    Parse parameters from skill's interaction model 🏃‍♂️🏃‍♀
    * (Outside this repo) added sex as an auto-delegated slot and created
    an intent for each run intensity in the skill's interaction model
    * Parse skill and intensity from the Alexa skill request and feed it
    into the temperature model
    * Add handlers for Amazon built-in intents

--- skill/src/main.rs
+++ skill/src/main.rs
@@ -5,12 +5,20 @@ mod outfit_intent;
 use alexa_sdk::request::IntentType;
 use alexa_sdk::{Request, Response};
 use anyhow::{bail, Result};
-use log::error;
+use log::{error, warn};
 
 fn handle_help(_req: &Request) -> Result<Response> {
     Ok(Response::simple(
-        "Help",
+        "Help 👟",
         "Outfit Picker can help you pick a running outfit. Try saying \"find me an outfit\".",
+    ))
+}
+
+fn handle_fallback(_req: &Request) -> Result<Response> {
+    Ok(Response::simple(
+        "Try something else 🏃",
+        "I didn't catch that. Try saying \"find me an outfit\" or \"what should I wear on my long \
+        run?\".",
     ))
 }
 
@@ -20,11 +28,14 @@ fn handle_cancel(_req: &Request) -> Result<Response> {
 
 async fn my_handler(req: Request) -> Result<Response> {
     match req.intent() {
+        IntentType::Cancel | IntentType::Stop => handle_cancel(&req),
+        IntentType::Fallback => handle_fallback(&req),
         IntentType::Help => handle_help(&req),
-        IntentType::Cancel => handle_cancel(&req),
         IntentType::User(_) => outfit_intent::handler(&req),
-        // TODO; handle other Amazon built-in intents
-        _ => handle_cancel(&req),
+        intent => {
+            warn!("Unknown intent {:?}", intent);
+            handle_fallback(&req)
+        }
     }
     .or_else(|err| {
         error!("Error: {}", err);
